fce27d57ddda1d8e8187e1f76460cb2c02d4bc0b
Eric <ericsouza94895@gmail.com>
2025-04-03 23:22:42 -0300

    feat: add UserService which has the main methods to manipulate users such as searching for the user by ID, searching for all users, saving, updating and deleting users

---
+++ src/main/java/com/senai/crud_spring/service/UsuariosService.java
@@ -0,0 +1,59 @@
+package com.senai.crud_spring.service;
+
+import com.senai.crud_spring.dtos.UsuariosDTO;
+import com.senai.crud_spring.model.Usuarios;
+import com.senai.crud_spring.repositories.UsuarioRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class UsuariosService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    private final ImagemService imagemService;
+
+    public void cadastrarUsuario(UsuariosDTO usuarioDTO, MultipartFile foto) {
+        try {
+            Usuarios usuario = new Usuarios();
+            usuario.setNome(usuarioDTO.nome());
+            usuario.setIdAcesso(UUID.fromString(usuarioDTO.idAcesso()));
+            usuario.setTelefone(usuarioDTO.telefone());
+            usuario.setCaminhoImagem(imagemService.storeFile(foto));
+            usuario.setEmail(usuarioDTO.email());
+            usuarioRepository.save(usuario);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void atualizarUsuario(UsuariosDTO usuariosDTO, Long id){
+        try {
+            Usuarios usuario = buscarUsuarioPeloId(id);
+            System.out.println(usuario.getNome());
+            usuario.setNome(usuariosDTO.nome());
+            usuario.setEmail(usuariosDTO.email());
+            usuario.setTelefone(usuariosDTO.telefone());
+            usuarioRepository.save(usuario);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deletarUsuario(long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuarios buscarUsuarioPeloId(long id){
+        return usuarioRepository.findById(id).orElseThrow();
+    }
+
+    public List<Usuarios> buscarTodosUsuarios(){
+        return usuarioRepository.findAll();
+    }
+}
