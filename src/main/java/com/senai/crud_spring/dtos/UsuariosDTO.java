a1fe5e29bc58771fc6cb0240438795e7182d66e4
Eric <ericsouza94895@gmail.com>
2025-04-04 23:57:06 -0300

    refactor: adding nnotations so that json ignores the image received in the request

--- src/main/java/com/senai/crud_spring/dtos/UsuariosDTO.java
+++ src/main/java/com/senai/crud_spring/dtos/UsuariosDTO.java
@@ -1,8 +1,12 @@
 package com.senai.crud_spring.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.multipart.MultipartFile;
 
 
-public record UsuariosDTO(String idAcesso, String nome, String telefone, String email) {
+public record UsuariosDTO(String idAcesso, String nome, String telefone, String email,
+                          @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) MultipartFile foto) {
+
 
 }
